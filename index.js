539396a7d06dbb8b178875f7e0f246ff8d337c7a
krizh4 <krishmikaabisheka1@gmail.com>
2022-09-07 15:40:17 +0530

    patch

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/whoami', (req, res, next) => {
+app.get('/api/whoami', (req, res) => {
   res.json({
-    ipaddress: req.socket.remoteAddress,
+    ipaddress: req.ip,
     language: req.headers["accept-language"],
     software: req.headers["user-agent"]
   });
